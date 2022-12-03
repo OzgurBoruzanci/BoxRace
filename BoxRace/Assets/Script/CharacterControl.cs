e39353f793ac66873dcd0af80aaa2bf07170db57
OzgurBoruzanci <ozgurboruzanc@gmail.com>
2022-12-03 03:25:02 +0300

    1.3

--- BoxRace/Assets/Script/CharacterControl.cs
+++ BoxRace/Assets/Script/CharacterControl.cs
@@ -2,57 +2,116 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class CharacterControl : MonoBehaviour
 {
-    List<GameObject> cubes;
+    public List<GameObject> cubes;
+    List<GameObject> symblCubes;
     float horizontal = 0;
     float distance = 0;
 
     Vector3 distanceBetween;
-    Vector3 newCube;
-    public GameObject obtained;
-    public GameObject cube;
-    public GameObject plane;
     
+    public GameObject player;
+    public GameObject cube;
+    public GameObject symbolCube;
+    public GameObject objeSymblCube;
+    public GameObject plane;
 
     
+
     void Start()
     {
-        distance=cube.transform.position.y-plane.transform.position.y;
+        distance= cube.transform.position.y - plane.transform.position.y;
+        //Debug.Log(distance + "   iki katı  ==" + distance * 2);
         
-
-        cubes=new List<GameObject>();
+        cubes =new List<GameObject>();
+        symblCubes =new List<GameObject>();
+       
         distanceBetween = transform.position - Camera.main.transform.position;
     }
 
-    //private void OnTriggerEnter(Collider other)
-    //{
 
-    //    if (other.gameObject.tag == "Cube")
-    //    {
-    //        Destroy(other.gameObject);
-    //        Instantiate(cube, newCube, transform.rotation);
-    //        cubes.Add(cube);
-    //        for (int i = 0; i < cubes.Count; i++)
-    //        {
-    //            transform.position = new Vector3(transform.position.x, transform.position.y + distance * 2, transform.position.z);
-    //            cubes[i].transform.position = transform.position;
-    //        }
-    //    }
-    //}
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.tag == "Cube")
+        if (other.gameObject.tag == "Cube")
         {
-            Destroy(collision.gameObject);
-            Instantiate(cube, newCube, transform.rotation);
-            cubes.Add(cube);
+            GameObject obje = other.gameObject;
+            if (cubes.Contains(other.gameObject)==false)
+            {
+                cubes.Add(obje);
+            }
             for (int i = 0; i < cubes.Count; i++)
             {
-                transform.position = new Vector3(transform.position.x, transform.position.y + 0.25f * 2, transform.position.z);
-                cubes[i].transform.position = transform.position;
+                cubes[i].transform.parent = transform;
+                transform.GetChild(1).transform.position = new Vector3(transform.position.x, distance, transform.position.z);
+                transform.GetChild(0).transform.position = new Vector3(transform.position.x, transform.position.y + distance * 2, transform.position.z);
+                
+            }
+            if (transform.childCount > 1)
+            {
+                for (int i = 2; i < transform.childCount; i++)
+                {
+
+                    float _ = transform.GetChild(i - 1).transform.position.y + distance * 2;
+                    transform.GetChild(i).transform.position = new Vector3(transform.position.x, _, transform.position.z);
+                    transform.GetChild(0).transform.position = new Vector3(transform.position.x, distance * 2 * i, transform.position.z);
+                }
             }
         }
+
+        if (other.gameObject.tag == "Obstacle")
+        {
+            
+            GameObject symblCube= Instantiate(symbolCube);
+            symblCubes.Add(symblCube);
+            for (int i = 2; i < symblCubes.Count; i++)
+            {
+                Destroy(symblCubes[0]);
+                symblCubes[i].transform.parent = objeSymblCube.transform;
+                symblCubes[1].transform.parent = objeSymblCube.transform;
+                symblCubes[1].transform.position = new Vector3(transform.position.x, distance, transform.position.z-0.25f);
+                float __ = symblCubes[i-1].transform.position.y + distance * 2;
+                symblCubes[i].transform.position = new Vector3(symblCubes[1].transform.position.x, __, symblCubes[1].transform.position.z);
+                
+            }
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            if (cubes.Count == 0)
+            {
+                transform.GetChild(0).transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+            }
+            else if (cubes.Count == 1)
+            {
+                transform.GetChild(1).transform.position = new Vector3(transform.position.x,distance,transform.position.z);
+                transform.GetChild(0).transform.position = new Vector3(transform.position.x, distance * 2, transform.position.z);
+            }
+            else if(cubes.Count > 1)
+            {
+                transform.GetChild(1).transform.position = new Vector3(transform.position.x, distance, transform.position.z);
+                transform.GetChild(0).transform.position = new Vector3(transform.position.x, distance * 2, transform.position.z);
+
+                if (transform.childCount > 1)
+                {
+                    for (int i = 2; i < transform.childCount; i++)
+                    {
+
+                        float _ = transform.GetChild(i - 1).transform.position.y + distance * 2;
+                        transform.GetChild(i).transform.position = new Vector3(transform.position.x, _, transform.position.z);
+                        transform.GetChild(0).transform.position = new Vector3(transform.position.x, distance * 2 * i, transform.position.z);
+                    }
+                }
+
+            }
+            symblCubes.Clear();
+        }
+
     }
 
 
@@ -63,16 +122,6 @@ public class CharacterControl : MonoBehaviour
     void Update()
     {
         MoveControl();
-        obtained.transform.position = new Vector3(transform.position.x, 0 + 0.25f, transform.position.z);
-        newCube = new Vector3(transform.position.x, 0.25f, transform.position.z);
-
-        //for (int i = 0; i < cubes.Count; i++)
-        //{
-        //    transform.position = new Vector3(transform.position.x, transform.position.y + distance * 2, transform.position.z);
-        //    Debug.Log(distance);
-
-        //    cubes[i].transform.position = new Vector3(transform.position.x, i * distance * 2, transform.position.z);
-        //}
 
     }
 
@@ -81,10 +130,10 @@ public class CharacterControl : MonoBehaviour
     void MoveControl()
     {
         horizontal = Input.GetAxis("Horizontal");
-        Vector3 vec = new Vector3(horizontal, 0, 2);
+        Vector3 vec = new Vector3(horizontal, 0, 1.5f);
         vec = transform.TransformDirection(vec);
         vec.Normalize();
-        transform.position += vec * Time.deltaTime * 3;
+        transform.position += vec * Time.deltaTime * 4;
     }
 
     void CameraControl()
@@ -92,3 +141,4 @@ public class CharacterControl : MonoBehaviour
         Camera.main.transform.position = transform.position - distanceBetween;
     }
 }
+
---
+++ BoxRace/Assets/Script/ObstacleControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleControl : MonoBehaviour
+{
+    CharacterControl characterControl;
+    public GameObject player;
+
+    private void Start()
+    {
+        characterControl = player.GetComponent<CharacterControl>();
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Cube")
+        {
+            characterControl.cubes.Remove(other.gameObject);
+            Destroy(other.gameObject);
+            //Debug.Log(characterControl.cubes.Count);
+
+        }
+    }
+
+}
